d6d7d674d850dd8ba70271a6a97836e5e2960acb
RiyaJanday <riyajanday1677@gmail.com>
2026-04-11 12:12:45 +0530

    Change done

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = dirname(__filename);
-dotenv.config({ path: join(__dirname, ".env") });
+dotenv.config();
 
 import express from "express";
 import http from "http";
